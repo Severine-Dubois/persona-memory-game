d529f44b47e6188439f5d0e92a8bc0d431b990b2
Severine Dubois <sevdbs@gmail.com>
2022-10-11 09:56:51 +0200

    installation du projet, des dépendances et ajout de quelques images

---
+++ persona-memory-game/src/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Memory Game</h1>
+      <button>Start a new game</button>
+    </div>
+  );
+}
+
+export default App;
