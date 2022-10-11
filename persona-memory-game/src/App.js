73d7522ed1b7d74019f84d5b505d033010fdeb76
Severine Dubois <sevdbs@gmail.com>
2022-10-11 10:15:13 +0200

    création de la fonction permettant de dupliquer les cartes et de les mélanger dans le tableau puis leur attribuer un id au hasard

--- persona-memory-game/src/App.js
+++ persona-memory-game/src/App.js
@@ -1,10 +1,38 @@
+import { useState } from 'react';
 import './App.scss';
 
+const cardImages = [
+  { "src": "/img/Emperor-0.webp" },
+  { "src": "/img/Empress-0.webp" },
+  { "src": "/img/Fool-0.webp" },
+  { "src": "/img/Hierophant-0.webp" },
+  { "src": "/img/Priestess-0.webp" },
+];
+
 function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  const shuffleCard = () => {
+    // On veut dupliquer chaque carte pour les avoir en double
+    const shuffle = [...cardImages, ...cardImages]
+      // on les mélange
+      .sort(() => Math.random() - 0.5)
+      // et pour chaque objet carte, on lui associe un id random
+      .map((card) => ({ ...card, id: Math.random() }))
+
+    setCards(shuffle);
+    // On utilise le setTurns car on va appeler la fonction shuffleCard à
+    // chaque nouvelle partie et donc remettre le compteur à 0
+    setTurns(0);
+  }
+
+  console.log(cards, turns);
+
   return (
     <div className="App">
       <h1>Memory Game</h1>
-      <button>Start a new game</button>
+      <button onClick={shuffleCard}>Start a new game</button>
     </div>
   );
 }
